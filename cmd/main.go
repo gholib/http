267925876ec8471e8f909a112d2a04970e0db4e2
gholib <gtrandnet@gmail.com>
2020-11-10 09:18:32 +0500

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -11,40 +11,23 @@ import (
 
 func main() {
 	host := "0.0.0.0"
-	port := "9999"
+	port := "8080"
 
 	if err := execute(host, port); err != nil {
-		os.Exit((1))
+		os.Exit(1)
 	}
 }
 
-func execute(host string, port string) (err error) {
+func execute(host string, port string) error {
 	srv := server.NewServer(net.JoinHostPort(host, port))
-	srv.Register("/", func(conn net.Conn) {
-		body := "Welcome to our wev-site"
-
-		_, err := conn.Write([]byte(
+	body := "hello"
+	srv.Register("/api/category{category}/{id}", func(req *server.Request) {
+		_, err := req.Conn.Write([]byte(
 			"HTTP/1.1 200 OK\r\n" +
 				"Content-Length: " + strconv.Itoa(len(body)) + "\r\n" +
 				"Content-Type: text/html\r\n" +
 				"Connection: close\r\n" +
-				"\r\n" +
-				body,
-		))
-		if err != nil {
-			log.Print(err)
-		}
-	})
-	srv.Register("/about", func(conn net.Conn) {
-		body := "About Golang Academy"
-
-		_, err := conn.Write([]byte(
-			"HTTP/1.1 200 OK\r\n" +
-				"Content-Length: " + strconv.Itoa(len(body)) + "\r\n" +
-				"Content-Type: text/html\r\n" +
-				"Connection: close\r\n" +
-				"\r\n" +
-				body,
+				"\r\n" + body,
 		))
 		if err != nil {
 			log.Print(err)
